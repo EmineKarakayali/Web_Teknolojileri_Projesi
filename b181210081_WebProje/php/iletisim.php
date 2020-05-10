b99e2bdafa6daf6dbf959b91e0a3a626364d3ff9
EmineKarakayali <63203404+EmineKarakayali@users.noreply.github.com>
2020-05-10 22:50:32 +0300

    WebTeknolojileri_Proje

---
+++ b181210081_WebProje/php/iletisim.php
@@ -0,0 +1,63 @@
+<?php
+
+   $isim= $_POST['ad'];
+
+   echo "İsim:  ";
+   echo $isim;
+   echo "<br>";
+
+ $soyisim= $_POST['soyad'];
+
+   echo "Soyisim:  ";
+   echo $soyisim;
+   echo "<br>";
+
+
+ $mail= $_POST['adresmail'];
+
+   echo "E-Mail:  ";
+   echo $mail;
+   echo "<br>";
+
+ $tarihGun= $_POST['tarihGun'];
+
+   echo "Gün:  ";
+   echo $tarihGun;
+   echo " ";
+  
+$tarihAy= $_POST['tarihAy'];
+
+   echo "Ay:  ";
+   echo $tarihAy;
+     echo " ";
+  
+  $tarihYil= $_POST['tarihYil'];
+
+   echo "Yıl:  ";
+   echo $tarihYil;
+   echo "<br>";
+  
+  
+ $sehir= $_POST['sehir'];
+
+   echo "Şehir:  ";
+   echo $sehir;
+   echo "<br>";
+
+    $cinsiyet= $_POST["cinsiyet"];
+
+   echo "Cinsiyet:  ";
+   echo $cinsiyet;
+   echo "<br>";
+
+         $mesaj= $_POST['mesaj'];
+
+   echo "Mesaj:  ";
+   echo $mesaj;
+   echo "<br>";
+
+     
+  
+  
+
+?>
